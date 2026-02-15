fbd82a1f151b9380de77d1e25d809c7ab4d6f035
Max Reiter (WSL) <87012332+Max-Reiter-math@users.noreply.github.com>
2026-02-15 19:12:19 +0100

    added experiments

--- sim/sim_presets/scaling.sh
+++ sim/sim_presets/scaling.sh
@@ -1,10 +1,40 @@
-/Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 10 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi1 -alpha 1.0 -dh 64 -dt 0.075 -e spiral -m linear_cg || true
-/Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 10 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi2 -alpha 1.0 -dh 64 -dt 0.075 -e spiral -m linear_dg || true
-/Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 8 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi3 -alpha 1.0 -dh 64 -dt 0.075 -e spiral -m linear_cg || true
-/Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 8 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi4 -alpha 1.0 -dh 64 -dt 0.075 -e spiral -m linear_dg || true
-/Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 6 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi5 -alpha 1.0 -dh 64 -dt 0.075 -e spiral -m linear_cg || true
-/Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 6 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi6 -alpha 1.0 -dh 64 -dt 0.075 -e spiral -m linear_dg || true
-/Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 4 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi7 -alpha 1.0 -dh 64 -dt 0.075 -e spiral -m linear_cg || true
-/Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 4 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi8 -alpha 1.0 -dh 64 -dt 0.075 -e spiral -m linear_dg || true
-/Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 2 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi9 -alpha 1.0 -dh 64 -dt 0.075 -e spiral -m linear_cg || true
-/Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 2 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi10 -alpha 1.0 -dh 64 -dt 0.075 -e spiral -m linear_dg
+# /Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 10 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi1 -alpha 1.0 -dh 64 -dt 0.075 -e spiral -m linear_cg || true
+# /Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 10 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi2 -alpha 1.0 -dh 64 -dt 0.075 -e spiral -m linear_dg || true
+# /Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 8 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi3 -alpha 1.0 -dh 64 -dt 0.075 -e spiral -m linear_cg || true
+# /Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 8 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi4 -alpha 1.0 -dh 64 -dt 0.075 -e spiral -m linear_dg || true
+# /Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 6 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi5 -alpha 1.0 -dh 64 -dt 0.075 -e spiral -m linear_cg || true
+# /Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 6 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi6 -alpha 1.0 -dh 64 -dt 0.075 -e spiral -m linear_dg || true
+# /Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 4 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi7 -alpha 1.0 -dh 64 -dt 0.075 -e spiral -m linear_cg || true
+# /Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 4 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi8 -alpha 1.0 -dh 64 -dt 0.075 -e spiral -m linear_dg || true
+# /Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 2 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi9 -alpha 1.0 -dh 64 -dt 0.075 -e spiral -m linear_cg || true
+# /Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 2 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi10 -alpha 1.0 -dh 64 -dt 0.075 -e spiral -m linear_dg || true
+/Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 10 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi11 -alpha 1.0 -dh 64 -dt 0.1 -e spiral -m linear_cg || true
+/Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 10 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi12 -alpha 1.0 -dh 64 -dt 0.1 -e spiral -m linear_dg || true
+/Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 8 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi13 -alpha 1.0 -dh 64 -dt 0.1 -e spiral -m linear_cg || true
+/Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 8 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi14 -alpha 1.0 -dh 64 -dt 0.1 -e spiral -m linear_dg || true
+/Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 6 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi15 -alpha 1.0 -dh 64 -dt 0.1 -e spiral -m linear_cg || true
+/Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 6 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi16 -alpha 1.0 -dh 64 -dt 0.1 -e spiral -m linear_dg || true
+/Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 4 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi17 -alpha 1.0 -dh 64 -dt 0.1 -e spiral -m linear_cg || true
+/Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 4 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi18 -alpha 1.0 -dh 64 -dt 0.1 -e spiral -m linear_dg || true
+/Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 2 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi19 -alpha 1.0 -dh 64 -dt 0.1 -e spiral -m linear_cg || true
+/Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 2 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi20 -alpha 1.0 -dh 64 -dt 0.1 -e spiral -m linear_dg || true
+/Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 10 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi21 -alpha 1.0 -dh 128 -dt 0.075 -e spiral -m linear_cg || true
+/Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 10 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi22 -alpha 1.0 -dh 128 -dt 0.075 -e spiral -m linear_dg || true
+/Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 8 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi23 -alpha 1.0 -dh 128 -dt 0.075 -e spiral -m linear_cg || true
+/Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 8 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi24 -alpha 1.0 -dh 128 -dt 0.075 -e spiral -m linear_dg || true
+/Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 6 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi25 -alpha 1.0 -dh 128 -dt 0.075 -e spiral -m linear_cg || true
+/Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 6 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi26 -alpha 1.0 -dh 128 -dt 0.075 -e spiral -m linear_dg || true
+/Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 4 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi27 -alpha 1.0 -dh 128 -dt 0.075 -e spiral -m linear_cg || true
+/Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 4 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi28 -alpha 1.0 -dh 128 -dt 0.075 -e spiral -m linear_dg || true
+/Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 2 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi29 -alpha 1.0 -dh 128 -dt 0.075 -e spiral -m linear_cg || true
+/Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 2 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi30 -alpha 1.0 -dh 128 -dt 0.075 -e spiral -m linear_dg || true
+/Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 10 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi31 -alpha 1.0 -dh 128 -dt 0.1 -e spiral -m linear_cg || true
+/Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 10 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi32 -alpha 1.0 -dh 128 -dt 0.1 -e spiral -m linear_dg || true
+/Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 8 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi33 -alpha 1.0 -dh 128 -dt 0.1 -e spiral -m linear_cg || true
+/Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 8 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi34 -alpha 1.0 -dh 128 -dt 0.1 -e spiral -m linear_dg || true
+/Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 6 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi35 -alpha 1.0 -dh 128 -dt 0.1 -e spiral -m linear_cg || true
+/Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 6 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi36 -alpha 1.0 -dh 128 -dt 0.1 -e spiral -m linear_dg || true
+/Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 4 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi37 -alpha 1.0 -dh 128 -dt 0.1 -e spiral -m linear_cg || true
+/Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 4 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi38 -alpha 1.0 -dh 128 -dt 0.1 -e spiral -m linear_dg || true
+/Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 2 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi39 -alpha 1.0 -dh 128 -dt 0.1 -e spiral -m linear_cg || true
+/Home/guests/reiter/anaconda3/envs/fenicsx09/bin/mpirun -n 2 python -m sim.run -vtx 0 -fsr 0.01 -msr 0.05 -ovw 1 -sid 4mpi40 -alpha 1.0 -dh 128 -dt 0.1 -e spiral -m linear_dg
